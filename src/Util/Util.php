bbe4a01978a396b8e8a4b6c387f82f61a90c5dff
John Pancoast <johnpancoaster@gmail.com>
2016-12-05 20:29:32 -0800

    Add more types to validate

--- src/Util/Util.php
+++ src/Util/Util.php
@@ -13,56 +13,92 @@ namespace Pancoast\Common\Util;
  * This should only contain the generic utility/helper functionality
  *
  * @author John Pancoast <johnpancoaster@gmail.com>
+ * @todo I'm sure the logic here can be fixed up but it works
  */
 class Util
 {
     /**
-     * Helper to validate that a value for a field is of a certain type
+     * Is $value of $type
      *
-     * @param mixed $value
-     * @param mixed $type
-     * @param null $field
+     * @param mixed         $value
+     * @param string|array  $type or an array of acceptable types
      *
-     * @throws \InvalidArgumentException If value not expected type
+     * @return bool
      */
-    public static function validateType($value, $type, $field = null)
+    public static function isType($value, $type)
     {
+        // return value
         $isValid = false;
 
-        switch ($type) {
-            case 'array':
-                $isValid = is_array($value);
-                break;
-            case 'bool':
-                $isValid = is_bool($value);
-                break;
-            case 'int':
-                $isValid = is_int($value);
-                break;
-            case 'string':
-                $isValid = is_string($value);
-                break;
+        // allow $type to be an array of types or create as array
+        $types = is_array($type) ? $type : [$type];
+
+        foreach ($types as $t) {
+            // are we checking that $value is an instance of $type?
+            // this should only be checked *after* all other type checks
+            // to avoid potential class collisions with the string types
+            $typeIsClass = class_exists($t);
+
+            switch ($t) {
+                case null:
+                    $isValid = is_null($value);
+                    break;
+                case 'object':
+                    $isValid = is_object($value);
+                    break;
+                case 'array':
+                    $isValid = is_array($value);
+                    break;
+                case 'bool':
+                case 'boolean':
+                    $isValid = is_bool($value);
+                    break;
+                case 'int':
+                case 'integer':
+                    $isValid = is_int($value);
+                    break;
+                case 'string':
+                    $isValid = is_string($value);
+                    break;
+                default:
+                    // one final check for type check on class
+                    if ($typeIsClass) {
+                        $isValid = is_object($value) && $value instanceof $t;
+                        break;
+                    }
+
+                    // if we got this far, the user passed the wrong type
+                    throw new \InvalidArgumentException('$type must be a valid type or an array of valid types');
+            }
+
+            if ($isValid) {
+                return $isValid;
+            }
         }
 
-        if (!$isValid) {
-            if ($field) {
-                throw new \InvalidArgumentException(
-                    sprintf(
-                        'Expected type %s for "%s". Received value "%s".',
-                        $type,
-                        $field,
-                        $value
-                    )
-                );
-            } else {
-                throw new \InvalidArgumentException(
-                    sprintf(
-                        'Expected type %s. Received value "%s".',
-                        $type,
-                        $value
-                    )
-                );
-            }
+        return false;
+    }
+
+    public static function validateType($value, $type)
+    {
+        if (!self::isType($value, $type)) {
+            // normalize the types for printing
+            $printTypes = array_map(function($t){
+                switch ($t) {
+                    case null:
+                        return 'null';
+                    case class_exists($t):
+                        return sprintf("'%s'", $t);
+                    default:
+                        return $t;
+                }
+            }, is_array($type) ? $type : [$type]);
+
+            throw new \InvalidArgumentException(sprintf(
+                '$value expected to be one of type [%s]. Received type %s.',
+                implode(', ', $printTypes),
+                is_object($value) ? get_class($value) : gettype($value)
+            ));
         }
     }
 }
